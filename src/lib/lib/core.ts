551bebdb97509c850955cab0b0bd9049e8b8f011
tanbo <464328895@qq.com>
2019-09-26 17:47:42 +0800

    feat:颜色计算完成

--- src/lib/lib/core.ts
+++ src/lib/lib/core.ts
@@ -1,7 +1,13 @@
 import { template } from './template';
+import {
+  any2Hsl,
+  hsl2Hex, hsl2Hsv,
+  hsl2Rgb, hsv2Hex, rgb2Hsv
+} from '@tanbo/color';
 
 export interface Options {
   colors?: string[];
+  value?: string;
 }
 
 export class Core {
@@ -9,7 +15,7 @@ export class Core {
 
   set value(v: string) {
     this._value = v;
-
+    this.render(v);
   }
 
   get value() {
@@ -18,6 +24,15 @@ export class Core {
 
   private _value = '#fff';
   private container: HTMLElement;
+  private valueViewer: HTMLElement;
+  private palette: HTMLElement;
+  private palettePoint: HTMLElement;
+  private hue: HTMLElement;
+  private huePoint: HTMLElement;
+
+  private hslInputs: HTMLInputElement[];
+  private rgbInputs: HTMLInputElement[];
+  private hexInput: HTMLInputElement;
 
   constructor(selector: string | HTMLElement, options: Options = {}) {
     if (typeof selector === 'string') {
@@ -49,9 +64,41 @@ export class Core {
     }
 
     this.container.appendChild(this.host);
+    this.valueViewer = this.host.querySelector('.tanbo-color-picker-value');
+    this.palette = this.host.querySelector('.tanbo-color-picker-palette');
+    this.palettePoint = this.host.querySelector('.tanbo-color-picker-palette-point');
+    this.hue = this.host.querySelector('.tanbo-color-picker-hue-pointer-wrap');
+    this.huePoint = this.host.querySelector('.tanbo-color-picker-hue-pointer');
+
+    this.hslInputs = Array.from(this.host.querySelectorAll('.tanbo-color-picker-hsl input'));
+    this.rgbInputs = Array.from(this.host.querySelectorAll('.tanbo-color-picker-rgb input'));
+    this.hexInput = this.host.querySelector('.tanbo-color-picker-hex input');
+
+    this.value = options.value || '#00f';
   }
 
   private render(color: string) {
+    const hsl = any2Hsl(color);
+    if (hsl === 'unknown') {
+      return;
+    }
+    const rgb = hsl2Rgb(hsl);
+    const hsb = hsl2Hsv(hsl);
+    const hex = hsl2Hex(hsl);
 
+    this.hslInputs[0].value = hsl.h + '';
+    this.hslInputs[1].value = hsl.s + '';
+    this.hslInputs[2].value = hsl.l + '';
+
+    this.rgbInputs[0].value = rgb.r + '';
+    this.rgbInputs[1].value = rgb.g + '';
+    this.rgbInputs[2].value = rgb.b + '';
+
+    this.hexInput.value = hex;
+    this.valueViewer.style.background = color;
+    this.palette.style.background = `linear-gradient(to right, #fff, hsl(${hsb.h}, 100%, 50%))`;
+    this.palettePoint.style.left = `calc(${hsb.s}% - 6px)`;
+    this.palettePoint.style.top = `calc(${100 - hsb.v}% - 6px)`;
+    this.huePoint.style.top = `calc(${hsb.h / 360 * 100}% - 4px)`;
   }
 }
--- src/lib/lib/template.ts
+++ src/lib/lib/template.ts
@@ -1,31 +1,27 @@
 export const template = `
 <div class="tanbo-color-picker-viewer">
   <div class="tanbo-color-picker-palette">
-    <div class="tanbo-color-picker-pick-wrap">
-      <div class="tanbo-color-picker-pick"></div>
-    </div>
+    <div class="tanbo-color-picker-palette-point"></div>
   </div>
   <div class="tanbo-color-picker-tools">
     <div class="tanbo-color-picker-value"></div>
     <div class="tanbo-color-picker-hue-bar">
-      <div class="tanbo-color-picker-hue-pointer-wrap">
-        <div class="tanbo-color-picker-hue-pointer"></div>
-      </div>
+      <div class="tanbo-color-picker-hue-pointer"></div>
     </div>
   </div>
 </div>
 <div class="tanbo-color-picker-inputs">
-  <div>
+  <div class="tanbo-color-picker-hsl">
     <div>H <input type="number"></div>
     <div>S <input type="number"></div>
     <div>L <input type="number"></div>
   </div>
-  <div>
+  <div class="tanbo-color-picker-rgb">
     <div>R <input type="number"></div>
     <div>G <input type="number"></div>
     <div>B <input type="number"></div>
   </div>
-  <div>
+  <div class="tanbo-color-picker-hex">
     <div>HEX <input type="text"></div>
   </div>
 </div>
