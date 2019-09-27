6247858d7ad026983ba16ed9a10ae6ce6a14ddcf
tanbo <464328895@qq.com>
2019-09-27 10:36:13 +0800

    feat:打包优化

--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,5 @@ const gulpSourceMap = require('gulp-sourcemaps');
 gulp.task('copy', function () {
   return gulp.src('./src/lib/assets/**/*').pipe(gulp.dest('./bundles/scss/'));
 });
-gulp.task('scss', function () {
-  return gulp.src(['./src/lib/assets/index.scss'])
-    .pipe(gulpSourceMap.init())
-    .pipe(gulpSass())
-    .pipe(gulpAutoPrefix())
-    .pipe(gulpConcat({
-      path: 'index.min.css'
-    }))
-    .pipe(gulpCssMin())
-    .pipe(gulp.dest('./bundles/'));
-});
 
-gulp.task('default', gulp.series('copy', 'scss'));
+gulp.task('default', gulp.series('copy'));
