cada4add58362553efae7c406e37be86d0999a15
tanbo <464328895@qq.com>
2019-10-09 11:58:24 +0800

    chore:去掉不需要的类库

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,4 @@
 const gulp = require('gulp');
-const gulpSass = require('gulp-sass');
-const gulpAutoPrefix = require('gulp-autoprefixer');
-const gulpCssMin = require('gulp-cssmin');
-const gulpConcat = require('gulp-concat');
-const gulpSourceMap = require('gulp-sourcemaps');
 
 gulp.task('copy', function () {
   return gulp.src('./src/lib/assets/**/*').pipe(gulp.dest('./bundles/scss/'));
